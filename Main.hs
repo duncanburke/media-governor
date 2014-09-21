b91cb9de2ac79f390dab0472913db8780e6442a0
Duncan Burke <duncankburke@gmail.com>
2014-09-21 15:30:29 +1000

    Main: added Main

---
+++ Main.hs
@@ -0,0 +1,19 @@
+{-
+Copyright (c) 2014 Duncan Burke <duncankburke@gmail.com>
+
+This software may be modifid and distributed under the terms
+of the MIT license. See the LICENSE file for details.
+-}
+
+{- |
+   module      : Main
+   copyright   : (c) Duncan Burke
+   license     : MIT
+
+   maintaner   : Duncan Burke <duncankburke@gmail.cmo>
+   stability   : unstable
+-}
+module Main where
+
+main :: IO ()
+main = return ()
